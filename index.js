d2b69f7d04e45632e8ad73e9e306a097f7d5a9f4
Ken <kabutukenneth3@gmail.com>
2023-04-09 18:55:09 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = bob;
+
+
+
+function upperCaseCustomerName(){
+    return CustomerName = customerName.toUpperCase();
+}
+
+
+ function setBestCustomer() {
+    bestCustomer = 'not bob';
+ }
+
+ overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+ }
+
+
+ const leastFavoriteCustomer = ken;
+
+ function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = carol;
+ }
+
+
